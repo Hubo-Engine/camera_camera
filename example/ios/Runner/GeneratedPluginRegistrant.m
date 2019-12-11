4e07929e159b3fed14e014d59e3dbd0c5939246c
LucazzP <lucashpolazzo2013@gmail.com>
2019-12-11 15:44:06 -0300

    Atualizado packages

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,13 +3,48 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<camera/CameraPlugin.h>)
 #import <camera/CameraPlugin.h>
+#else
+@import camera;
+#endif
+
+#if __has_include(<flutter_native_image/FlutterNativeImagePlugin.h>)
 #import <flutter_native_image/FlutterNativeImagePlugin.h>
+#else
+@import flutter_native_image;
+#endif
+
+#if __has_include(<image_cropper/ImageCropperPlugin.h>)
 #import <image_cropper/ImageCropperPlugin.h>
+#else
+@import image_cropper;
+#endif
+
+#if __has_include(<native_device_orientation/NativeDeviceOrientationPlugin.h>)
 #import <native_device_orientation/NativeDeviceOrientationPlugin.h>
-#import <path_provider/PathProviderPlugin.h>
+#else
+@import native_device_orientation;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<screen/ScreenPlugin.h>)
 #import <screen/ScreenPlugin.h>
-#import <video_player/VideoPlayerPlugin.h>
+#else
+@import screen;
+#endif
+
+#if __has_include(<video_player/FLTVideoPlayerPlugin.h>)
+#import <video_player/FLTVideoPlayerPlugin.h>
+#else
+@import video_player;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
