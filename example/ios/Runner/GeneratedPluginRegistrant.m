bb40424a6d29592cc06e2b62e96e46ddbba66a4b
LucazzP <lucashpolazzo2013@gmail.com>
2019-12-12 11:48:40 -0300

    Removed video

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -40,12 +40,6 @@
 @import screen;
 #endif
 
-#if __has_include(<video_player/FLTVideoPlayerPlugin.h>)
-#import <video_player/FLTVideoPlayerPlugin.h>
-#else
-@import video_player;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
@@ -55,7 +49,6 @@
   [NativeDeviceOrientationPlugin registerWithRegistrar:[registry registrarForPlugin:@"NativeDeviceOrientationPlugin"]];
   [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
   [ScreenPlugin registerWithRegistrar:[registry registrarForPlugin:@"ScreenPlugin"]];
-  [FLTVideoPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTVideoPlayerPlugin"]];
 }
 
 @end
--- lib/camera_camera.dart
+++ lib/camera_camera.dart
@@ -1,5 +1,4 @@
 library camera_camera;
 
 export 'package:camera_camera/page/camera.dart';
-export 'package:camera_camera/page/video.dart';
 export 'package:camera_camera/shared/widgets/focus_widget.dart';
